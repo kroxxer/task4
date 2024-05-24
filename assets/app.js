61dbf0ea2faaaf137b1ef547cb7fd9be13dbaf44
Aboba <valikcrazy@gmail.com>
2024-05-24 13:57:45 +0300

    aboba

--- assets/app.js
+++ assets/app.js
@@ -5,14 +5,6 @@ import './bootstrap.js';
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
-import "../node_modules/bootstrap/scss/bootstrap.scss";
-
-import 'bootstrap';
-import bsCustomFileInput from 'bs-custom-file-input';
-bsCustomFileInput.init();
-
-var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
-var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
-    return new bootstrap.Popover(popoverTriggerEl)
-})
+import './styles/app.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
