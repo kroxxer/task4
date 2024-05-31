98dd7c4453ed4014010ad77e63ee8a9bc09e55b1
Aboba <valikcrazy@gmail.com>
2024-05-31 12:20:59 +0300

    aboba

--- src/Security/AuthenticationFailureHandler.php
+++ src/Security/AuthenticationFailureHandler.php
@@ -2,16 +2,15 @@
 
 namespace App\Security;
 
-use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Core\Exception\BadCredentialsException;
-use Symfony\Component\Security\Core\Exception\CustomUserMessageAccountStatusException;
 use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
 use Symfony\Component\Security\Http\Authentication\AuthenticationFailureHandlerInterface;
+use Symfony\Component\Security\Http\SecurityRequestAttributes;
 
 class AuthenticationFailureHandler implements AuthenticationFailureHandlerInterface
 {
@@ -24,7 +23,7 @@ class AuthenticationFailureHandler implements AuthenticationFailureHandlerInterf
     {
         if ($exception instanceof BadCredentialsException)
             $exception = new CustomUserMessageAuthenticationException('Don\'t have matched email');
-        $request->getSession()->set(Security::AUTHENTICATION_ERROR, $exception);
+        $request->getSession()->set(SecurityRequestAttributes::AUTHENTICATION_ERROR, $exception);
         return new RedirectResponse($this->urlGenerator->generate('app_user_index'));
     }
 }
